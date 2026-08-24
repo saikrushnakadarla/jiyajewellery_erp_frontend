187ff8244c9e215a574536024cc7d2c562d94f6b
Saikrushna <kadarlasaikrushna01@gmail.com>
2026-08-24 13:25:13 +0530

    saikrushna

--- src/Components/Modules/Transactions/ReturnMainStock/ProductDetails.js
+++ src/Components/Modules/Transactions/ReturnMainStock/ProductDetails.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState, useRef } from 'react';
 import { Col, Row, Button, Dropdown, DropdownButton, Modal } from 'react-bootstrap';
 import InputField from './InputfieldSales';
